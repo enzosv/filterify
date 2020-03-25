1fb6bb0b279806886962d1e4c7271af6ffd5110f
Enzo Vergara <vergara.enzo@gmail.com>
2020-03-25 12:09:46 +0800

    use local storage instead of cookies for token

--- app/js/models/token.js
+++ app/js/models/token.js
@@ -1,45 +1,18 @@
-var TOKEN
-saveToken()
+const TOKEN = processToken()
 
-function saveToken(){
-    var urlParams = new URLSearchParams(window.location.hash)
-    history.pushState("", document.title, window.location.pathname +
+function processToken() {
+  var urlParams = new URLSearchParams(window.location.hash)
+  history.pushState("", document.title, window.location.pathname +
     window.location.search)
-    var access_token = urlParams.get('#access_token')
-    if(access_token == null) {
-    TOKEN = readCookie("access_token")
-        return
-    }
-    TOKEN = access_token
-    createCookie("access_token", access_token, urlParams.get('expires_in'))
-}
-
-function createCookie(name, value, expiration_ms) {
-  if (expiration_ms) {
-    var date = new Date();
-    date.setTime(date.getTime() + expiration_ms);
-    var expires = "; expires=" + date.toGMTString();
-  } else var expires = "";
-  document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  var access_token = urlParams.get('#access_token')
+  if (access_token != null) {
+    localStorage.setItem('access_token', access_token)
   }
-  return null;
-}
-
-function eraseCookie(name) {
-  createCookie(name, "", -1);
+  return access_token || localStorage.getItem('access_token')
 }
 
 function handleUnauthorized(xhr, status, error) {
   if (xhr.status === 401) {
-      window.location.href = window.location.href.replace("/app","")
+    window.location.href = window.location.href.replace("/app", "")
   }
 }
